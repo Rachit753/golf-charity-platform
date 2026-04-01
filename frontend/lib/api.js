3615162b768134f9e1fdfe4c72d48281d337e94c
Rachit753 <chauhanrachit02@gmail.com>
2026-04-01 20:05:55 +0530

    Updated frontend to use production backend API

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://golf-charity-backend-fh0i.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
